0b2bc8f8fd7e637064cd3255c54bc93e06309d0a
Caroline Orsi <caroline.orsi@gmail.com>
2014-11-06 10:31:50 -0800

    Adjusted search buffers.

--- static/js/directions.js
+++ static/js/directions.js
@@ -38,34 +38,34 @@ function getPlaces(polyline, place) {
 	// Decode polyline from Google Directions response:
 	var decodedPolyline = google.maps.geometry.encoding.decodePath(polyline);
 
-	pointsInPolyline = decodedPolyline.length
-	for (var i = 0; i < pointsInPolyline; i = i + 5) {
+	pointsInPolyline = decodedPolyline.length;
+	increment = Math.ceil(pointsInPolyline / 10);
+	for (var i = 0; i < pointsInPolyline; i = (i + increment)) {
 		// var marker = new google.maps.Marker({
 		//     position: decodedPolyline[i],
 		//     map: map
 		// });
-
-		getPlacesByPoint(decodedPolyline[i], place);
-
+		
+		getPlacesByPoint(decodedPolyline[i], place, i);
+		
 		var circle = {
 			fillColor: '#000',
 			fillOpacity: 0.3,
 			strokeWeight: 0.3,
 			map: map,
 			center: decodedPolyline[i],
-			radius: 500
+			radius: 10000
 		}
 		pointRadius = new google.maps.Circle(circle);		
 	};
 }
 
-function getPlacesByPoint(location, place) {
-	var placesList = [];
+function getPlacesByPoint(location, place, i) {
 
 	// Create Places request
 	var request = {
 		location: location,
-		radius: 500,
+		radius: 50000,
 		rankby: 'distance',
 		keyword: place
 	}
@@ -74,10 +74,11 @@ function getPlacesByPoint(location, place) {
 	// if (status != google.maps.places.PlacesServiceStatus.OK) {
 	// 	console.log(status);
 	// }
+		// console.log(status);
+		// console.log(i);
 		if (status == google.maps.places.PlacesServiceStatus.OK) {
 			for (var j = 0; j < results.length; j++) {
 				// console.log(results[j].name);
-				placesList.push(results[j].name);
 				var marker = new google.maps.Marker({
 					position: results[j].geometry.location,
 					map: map
@@ -92,6 +93,5 @@ function getPlacesByPoint(location, place) {
 			}
 		}
 	});
-	console.log(placesList);
 
 }
